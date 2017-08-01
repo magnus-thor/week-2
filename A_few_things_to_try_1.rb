7a153ed43876a1a56656f4f14f38c106dad5b274
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-01 17:56:58 +0200

    Added the tough question

--- A_few_things_to_try_1.rb
+++ A_few_things_to_try_1.rb
@@ -1,11 +1,15 @@
 year = 365
 day = 24
 hour = 60
-min = 100
+min = 60
 # Hours in a year
 puts "There are #{year*day} hours in a year"
 # Minutes in a decade
 puts "and #{year*10*day*hour} minutes in a decade"
 # How old i am in seconds
 puts "I am #{year*41*day*hour*min} seconds old"
+# If i am 1246 million seconds old, how old am I?
+puts "If I am 1246 million seconds old I am #{1246000000/60/60/24/365} years old"
+
+
 
