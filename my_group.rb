175f2e43fbb6ffbd1ea1de8969c69f700d53963c
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-02 00:08:34 +0200

    Added a iterate to the program

--- my_group.rb
+++ my_group.rb
@@ -3,3 +3,7 @@ my_group = [
   person_2 = {name: "Anna", gender: "woman", age: 34}, 
   person_3 = {name: "Billy", gender: "man", age: 49}
   ]
+
+my_group.each do |person|
+  puts "#{person[:name]} is a #{person[:age]} years old #{person[:gender]}"
+end
