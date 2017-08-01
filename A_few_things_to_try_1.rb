f37454333498f8ebcab5fdefbef91f2fa1c3ed65
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-01 17:30:55 +0200

    The easy questions

---
+++ A_few_things_to_try_1.rb
@@ -0,0 +1,11 @@
+year = 365
+day = 24
+hour = 60
+min = 100
+# Hours in a year
+puts "There are #{year*day} hours in a year"
+# Minutes in a decade
+puts "and #{year*10*day*hour} minutes in a decade"
+# How old i am in seconds
+puts "I am #{year*41*day*hour*min} seconds old"
+
