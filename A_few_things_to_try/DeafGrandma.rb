48ca43f647494612ca55c72fd8136c634b1cd100
magnus-thor <magnus.thor.ar@gmail.com>
2017-08-03 13:04:32 +0200

    Extended Deaf grandma to pretend not to hear you

--- A_few_things_to_try/DeafGrandma.rb
+++ A_few_things_to_try/DeafGrandma.rb
@@ -1,14 +1,21 @@
 # Deaf Grandma
 
 question = ""
+bye_3times = 0
 
-while question != "BYE"
+while bye_3times < 3
   puts "Say something to grandma:"
   question = gets.chomp
   if question != question.upcase
     puts "HUH?!, SPEAK UP, SONNY!"
+    bye_3times = 0
   elsif (question == question.upcase && question != "BYE")
     year = rand 1930..1951
     puts "NO, NOT SINCE #{year}!"
+    bye_3times = 0
+  else
+    if question == "BYE"
+      bye_3times += 1    
+    end
   end
 end
